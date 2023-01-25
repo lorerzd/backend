6e83ed2dfc382e5080e2bcb2be5f426d5a409c21
Josue Torres <josuetolvera@gmail.com>
2023-01-25 16:04:59 -0600

    creación de modelo de producto

---
+++ naomdb/src/test/java/org/generation/naomdb/NaomdbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.naomdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaomdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
