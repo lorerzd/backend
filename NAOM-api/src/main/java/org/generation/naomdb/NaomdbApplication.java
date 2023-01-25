e85af2a78d133f2ee46b462feac93c4bf9edb09f
Josue Torres <josuetolvera@gmail.com>
2023-01-25 16:10:13 -0600

    Creación del modelo de productos

---
+++ NAOM-api/src/main/java/org/generation/naomdb/NaomdbApplication.java
@@ -0,0 +1,13 @@
+package org.generation.naomdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaomdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaomdbApplication.class, args);
+	}
+
+}
